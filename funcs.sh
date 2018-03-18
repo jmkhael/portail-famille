4b3996a2667e28f328c63192e9f4abc66200ced2
Johnny Mkhael <johnny.mkhael@gmail.com>
2018-03-18 19:22:30 +0100

    Adding funcs to ease lookups

---
+++ funcs.sh
@@ -0,0 +1,24 @@
+function findBillsPerDate()
+{
+
+if [ $# -ne 1 ]
+  then
+    usage $FUNCNAME "month/year";
+    echo "e.g: $FUNCNAME 2018"
+    echo "e.g: $FUNCNAME 01/2018"
+    echo "e.g: $FUNCNAME 29/01/2018"
+  else
+    year=$1
+    for f in $(find . -name 'bill*.json'); do 
+
+      matches=$(jq .billingDate $f | grep $year | wc -l);
+
+      if [ $matches -eq 1 ]
+      then
+        echo $f
+	amounts=$(jq '.items[] | select(.id | contains("LOUKAS")) | .amount' $f)
+	echo $amounts
+      fi
+    done
+fi
+}
